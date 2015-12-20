73bd090948c6fca12b08839bc3001fa8d97890b2
Zmiecer <dmitrii.bylinovich@gmail.com>
2015-12-20 19:35:38 +0300

    Corrections

--- 2 course/Java/Lab12/src/by/bsu/zmiecer/MyApplet.java
+++ 2 course/Java/Lab12/src/by/bsu/zmiecer/MyApplet.java
@@ -1,6 +1,6 @@
-import javax.swing.*;
+package by.bsu.zmiecer;import javax.swing.*;import javax.swing.JApplet;import javax.swing.JButton;import javax.swing.JPanel;
 import java.awt.*;
-import java.awt.event.*;
+import java.awt.Graphics;import java.awt.event.*;import java.awt.event.KeyEvent;import java.awt.event.KeyListener;import java.awt.event.MouseEvent;import java.awt.event.MouseListener;import java.awt.event.MouseMotionListener;import java.lang.Override;import java.lang.StringBuffer;
 
 /**
  * Created by Zmiecer on 29.11.2015.
---
+++ 2 course/Java/Lab15/src/by/bsu/zmiecer/Frame.java
@@ -0,0 +1,10 @@
+package by.bsu.zmiecer;
+
+import javax.swing.*;
+
+/**
+ * Created by Zmiecer on 20.12.2015.
+ */
+public class Frame extends JFrame {
+
+}
