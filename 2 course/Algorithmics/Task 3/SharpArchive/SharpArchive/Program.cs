285b20fc3de5c38c866495a990c8caa91301b0f5
Zmiecer <dmitrii.bylinovich@gmail.com>
2016-05-15 11:17:12 +0300

    SharpArchive

---
+++ 2 course/Algorithmics/Task 3/SharpArchive/SharpArchive/Program.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+
+namespace SharpArchive
+{
+    class Program
+    {
+        public const bool RIGHT = true;
+        public const bool LEFT = false;
+        public static Random rand = new Random();
+        public static List<int> ints = new List<int>();
+        public class Treap
+        {
+            private List<Node> list;
+            private int count;
+            private int root;
+
+            public Treap()
+            {
+                list = new List<Node>();
+                count = 0;
+                root = -1;
+            }
+
+            public void Add(int value)
+            {
+                list.Add(new Node(value, 1, rand.Next()));
+                SetRoot(Merge(root, GetSize()));
+                IncreaseSize();
+            }
+
+            public int GetRoot()
+            {
+                return this.root;
+            }
+
+            public void SetRoot(int root)
+            {
+                this.root = root;
+            }
+
+            public int GetSize()
+            {
+                return this.count;
+            }
+
+            public void SetSize(int size)
+            {
+                this.count = size;
+            }
+
+            public void IncreaseSize()
+            {
+                this.count++;
+            }
+
+            public int GetKeyByIndex(int index)
+            {
+                return (index == -1) ? 0 : GetNodeByIndex(index).Key;
+            }
+
+            public Node GetNodeByIndex(int index)
+            {
+                return this.list[index];
+            }
+
+            private int Merging(int first, int second, bool rightChild)
+            {
+                int done = (rightChild) ? GetNodeByIndex(first).Right = Merge(GetNodeByIndex(first).Right, second) : GetNodeByIndex(first).Left = Merge(second, GetNodeByIndex(first).Left);
+                Recalc(first);
+                return first;
+            }
+
+            public int Merge(int left, int right)
+            {
+                return (left == -1 || right == -1) ? ((left == -1) ? right : left) : (GetNodeByIndex(left).GetPriority() < GetNodeByIndex(right).GetPriority()) ?  Merging(left, right, RIGHT) : Merging(right, left, LEFT);
+            }
+
+            private int Splitting(int index, int cuttingLine, ref int first, ref int second, bool rightChild)
+            {
+                int k = (rightChild) ? (Split(GetNodeByIndex(index).Right, cuttingLine - GetKeyByIndex(GetNodeByIndex(index).Left) - 1, ref first, ref second)) : (Split(GetNodeByIndex(index).Left, cuttingLine, ref second, ref first));
+                GetNodeByIndex(index).SetChild(rightChild, first);
+                first = index;
+                Recalc(index);
+                return 0;
+            }
+
+            public int Split(int index, int cuttingLine, ref int left, ref int right)
+            {
+                return (index == -1)? (left = right = -1) : (GetKeyByIndex(GetNodeByIndex(index).Left) < cuttingLine) ? Splitting(index, cuttingLine, ref left, ref right, RIGHT) : Splitting(index, cuttingLine, ref right, ref left, LEFT);
+            }
+
+            private void Recalc(int index)
+            {
+                GetNodeByIndex(index).Key = GetKeyByIndex(GetNodeByIndex(index).Left) + GetKeyByIndex(GetNodeByIndex(index).Right) + 1;
+            }
+
+            public void WriteAnswer()
+            {
+                WriteAnswer(root);
+            }
+
+            private int WriteAnswer(int index)
+            {
+                return (index == -1) ? 0 : WritingAnswer(index);
+            }
+
+            private int WritingAnswer(int index)
+            {
+                WriteAnswer(GetNodeByIndex(index).Left);
+                ints.Add(GetNodeByIndex(index).GetValue());
+                WriteAnswer(GetNodeByIndex(index).Right);
+                return 0;
+            }
+
+            public void Request(int start, int finish)
+            {
+                int left = -1, right = -1;
+                Split(root, finish, ref root, ref right);
+                Split(root, start - 1, ref left, ref root);
+                SetRoot(Merge(root, left));
+                SetRoot(Merge(root, right));
+            }
+
+            public class Node
+            {
+                private int value;
+                private int key;
+                private int priority;
+                private int left;
+                private int right;
+
+                public Node(int value, int key, int priority)
+                {
+                    this.value = value;
+                    this.key = key;
+                    this.priority = priority;
+                    left = -1;
+                    right = -1;
+                }
+
+                public int Key
+                {
+                    get
+                    {
+                        return this.key;
+                    }
+                    set
+                    {
+                        this.key = value;
+                    }
+                }
+
+                public int GetValue()
+                {
+                    return this.value;
+                }
+
+                public int GetPriority()
+                {
+                    return this.priority;
+                }
+
+                public int SetChild(bool rightChild, int child)
+                {
+                    return (rightChild) ? this.right = child : this.left = child;
+                }
+
+                public int Left
+                {
+                    get
+                    {
+                        return this.left;
+                    }
+                    set
+                    {
+                        this.left = value;
+                    }
+                }
+
+                public int Right
+                {
+                    get
+                    {
+                        return this.right;
+                    }
+                    set
+                    {
+                        this.right = value;
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Treap treap = new Treap();
+            using (TextReader reader = File.OpenText(@"archive.in"))
+            {
+                int n, q;
+                string text = reader.ReadLine();
+                string[] bits = text.Split(' ');
+                n = int.Parse(bits[0]);
+                q = int.Parse(bits[1]);
+                for (int i = 1; i <= n; i++)
+                {
+                    treap.Add(i);
+                }
+                for (int i = 0; i < q; i++)
+                {
+                    text = reader.ReadLine();
+                    bits = text.Split(' ');
+                    int left = int.Parse(bits[0]);
+                    int right = int.Parse(bits[1]);
+                    treap.Request(left, right);
+                }
+            }
+            treap.WriteAnswer();
+            using (StreamWriter file = new StreamWriter(@"archive.out"))
+            {
+                foreach(int number in ints)
+                {
+                    file.Write(number + " ");
+                }
+            }
+        }
+    }
+}
